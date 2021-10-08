d2305ab37693ea77a936db75303aef6e4d836f98
Samiul Hoque <samiulh@andrew.cmu.edu>
2021-10-08 13:28:09 -0400

    Fixed syntax errors

--- meshNetworking/mpcSession.swift
+++ meshNetworking/mpcSession.swift
@@ -1,10 +1,10 @@
 import Foundation
 import MultipeerConnectivity
 
-class Session: MCSessionDelegate  {
-    private let peerID: MCPeerID(displayName: UIDevice.current.name)
+class Session: NSObject, MCSessionDelegate, MCNearbyServiceBrowserDelegate, MCNearbyServiceAdvertiserDelegate  {
+    private let peerID = MCPeerID(displayName: UIDevice.current.name)
     private let serviceString: String
-    private let session: MCSession?
+    private let session: MCSession
     private let nearbyServiceAdvertiser: MCNearbyServiceAdvertiser
     private let browser: MCNearbyServiceBrowser
     
@@ -12,44 +12,42 @@ class Session: MCSessionDelegate  {
         serviceString = service
         session = MCSession(peer: peerID, securityIdentity: nil,
             encryptionPreference: .required)
-        advertiser = MCNearbyServiceAdvertiser(peer: peerID
-            discoveryInfo: nil, discoveryInfo: nil, serviceType: serviceString)
+        nearbyServiceAdvertiser = MCNearbyServiceAdvertiser(peer: peerID,
+            discoveryInfo: nil, serviceType: serviceString)
 
         super.init()
         session.delegate = self
-        advertiser.delegate = self
+        nearbyServiceAdvertiser.delegate = self
     }
 
 
     // start advertising device and browsing for other devices
     func startDiscovery() {
-        mcAdvertiser.startAdvertisingPeer()
-        mcBrowser.startBrowsingForPeers()
+        nearbyServiceAdvertiser.startAdvertisingPeer()
     }
 
     // suspend advertising and browsing
     func suspendDiscovery() {
-        mcAdvertiser.stopAdvertisingPeer()
-        mcBrowser.stopBrowsingForPeers()
+        nearbyServiceAdvertiser.stopAdvertisingPeer()
     }
 
     // suspend advertising and browsing and disconnect from session
     func stopDiscovery() {
-        suspend()
-        mcSession.disconnect()
+        suspendDiscovery()
+        session.disconnect()
     }
 
-    // send data to all peers in the local network
-    func sendData(data: Data, peers: [MCPeerID], mode: MCSessionSendDataMode) {
+    // send data to a specific peer
+    func sendDataToPeer(data: Data, peers: [MCPeerID], mode: MCSessionSendDataMode) {
         do {
-            try mcSession.send(data, toPeers: peers, with: mode)
+            try session.send(data, toPeers: peers, with: mode)
         } catch let error {
             NSLog("Error sending data: \(error)")
         }
     }
     
-    // send data to specific peer in the local network
-    func sendDataToPeer() {
-        sendData(data: data, peers: mcSession.connectedPeers, mode: .reliable)
+    // send data to all peers
+    func sendDataToAllPeers(data: Data) {
+        sendDataToPeer(data: data, peers: session.connectedPeers, mode: .reliable)
     }
 }
